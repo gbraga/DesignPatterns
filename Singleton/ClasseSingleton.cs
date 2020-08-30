737d000b31c3531f98bc63d4f1f78680c89b8be9
Gabriel Braga <gabriel.braga@reserve.com.br>
2020-08-30 12:28:32 -0300

    implementação básica

---
+++ Singleton/ClasseSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class ClasseSingleton
+    {
+        private static ClasseSingleton _instance;
+
+        public Guid Id { get; private set; }
+
+        private ClasseSingleton()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static ClasseSingleton Instance()
+        {
+            if (_instance == null)
+                _instance = new ClasseSingleton();
+
+            return _instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImplementacaoBasica();
+        }
+
+        static void ImplementacaoBasica()
+        {
+            var instance = ClasseSingleton.Instance();
+            Console.WriteLine($"Primeira chamada: {instance.Id}");
+
+            var instance2 = ClasseSingleton.Instance();
+            Console.WriteLine($"Segunda chamada: {instance2.Id}");
+        }
+    }
+}
